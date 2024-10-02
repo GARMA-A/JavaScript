28ce9393b5fe65beafe038f91fc2b9cba8ead320
Girgis Emad <girgisemad74@gmail.com>
2024-10-02 23:45:49 +0300

    Add once function

--- deepJs/main.ts
+++ deepJs/main.ts
@@ -1,3 +1,18 @@
-let arr = [1, 2, 3, 4, 5];
+// const once = <FNType extends (...args: any[]) => any>(fn: FNType) => {
+//   let done = false;
+//   return ((...args: Parameters<FNType>) => {
+//     if (!done) {
+//       done = true;
+//       return fn(...args);
+//     }
+//   }) as FNType;
+// };
 
-Math.max(...arr); // 5
+
+
+
+
+
+
+
+
